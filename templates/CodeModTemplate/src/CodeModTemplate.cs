e9f84c7ad0bb45d1eda4a8f868a8ec819ac56d16
redleaf1221 <redleaf1221@outlook.com>
2026-03-22 21:17:31 +0800

    display name

--- templates/CodeModTemplate/src/CodeModTemplate.cs
+++ templates/CodeModTemplate/src/CodeModTemplate.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 
 namespace CodeModTemplate {
-    public class Main : SimpleModBehaviour {
+    public class CodeModTemplate : SimpleModBehaviour {
         public override void OnModLoaded() {
+            // Do something.
             Log("{{ModTitle}} is loaded!");
+        }
+
+        public override void OnModUnloaded() {
+            
         }
     }
 }
